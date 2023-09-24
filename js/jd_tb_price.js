81298eccf49218f7fd3d874af72f473dd8fe785a
Takina0 <142141536+Takina0@users.noreply.github.com>
2023-09-24 23:40:24 +0800

    Update jd_tb_price.js

--- js/jd_tb_price.js
+++ js/jd_tb_price.js
@@ -1,18 +1,8 @@
 /*
-
-【Loon】
-[Script]
-http-response ^https?://api\.m\.jd\.com/client\.action\?functionId=(wareBusiness|serverConfig|basicConfig) script-path=https://service.2ti.st/QuanX/Script/jd_tb_price/main.js, requires-body=true, timeout=10, tag=京东比价
-
-[MITM]
-hostname = api.m.jd.com
-*/
-/*
 JD | TB Price comparison
 by Small
 date 2021-06-25
 Thanks @yichahucha
-
 QX:
 ^https?://api\.m\.jd\.com/client\.action\?functionId=(wareBusiness|serverConfig|basicConfig) url script-response-body https://service.2ti.st/QuanX/Script/jd_tb_price/main.js
 
