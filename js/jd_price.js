219a6c59006ad8517ea21ced443737c352ef8f65
Takina0 <142141536+Takina0@users.noreply.github.com>
2023-09-24 15:28:39 +0800

    Update jd_price.js

--- js/jd_price.js
+++ js/jd_price.js
@@ -1,7 +1,7 @@
 /*
 【Loon】
 [Script]
-http-response ^https?://api\.m\.jd\.com/(client\.action|api)\?functionId=(wareBusiness|serverConfig|basicConfig|lite_wareBusiness|pingou_item) script-path=https://github.com/Takina0/Scripts/raw/main/js/jd_price2.js, requires-body=true, tag=京东比价
+http-response ^https?://api\.m\.jd\.com/(client\.action|api)\?functionId=(wareBusiness|serverConfig|basicConfig|lite_wareBusiness|pingou_item) script-path=https://github.com/Takina0/Scripts/raw/main/js/jd_price.js, requires-body=true, tag=京东比价
 
 [MITM]
 hostname = api.m.jd.com
